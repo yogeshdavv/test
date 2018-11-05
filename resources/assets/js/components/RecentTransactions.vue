fc504ca2e973f8158ee063e9cf13b3030d48c3ee
Daniël van Drunen <daniel@pixely.me>
2018-11-05 23:11:34 +0100

    Display dates of transactions in RecentTransactions component

--- resources/assets/js/components/RecentTransactions.vue
+++ resources/assets/js/components/RecentTransactions.vue
@@ -2,7 +2,8 @@
     <div class="box">
         <div class="box__section box__section--header">Recent Transactions</div>
         <div class="box__section row" v-for="transaction in transactions">
-            <div class="row__column">{{ transaction.description }}</div>
+            <div class="row__column row__column--compact">{{ new Date(transaction.happened_on).toISOString().slice(5, 10) }}</div>
+            <div class="row__column mx-15">{{ transaction.description }}</div>
             <div class="row__column row__column--compact" :class="{ 'color-green': transaction.type == 'earning', 'color-red': transaction.type == 'spending' }">{{ currency}} {{ new Intl.NumberFormat().format(transaction.amount / 100) }}</div>
         </div>
         <div class="box__section text-center" v-show="fetching">
