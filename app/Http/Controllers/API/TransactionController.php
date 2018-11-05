40987136c0fc8f608efdbffe6a0102ffc40664f6
Daniël van Drunen <daniel@pixely.me>
2018-11-05 17:24:12 +0100

    Through API, only show transactions of space you've selected

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -9,12 +9,13 @@ use Illuminate\Support\Facades\DB;
 class TransactionController extends Controller {
     public function index(Request $request) {
         $query = '
-            SELECT _type, id, happened_on, description, FORMAT(amount / 100, 2) AS formatted_amount
+            SELECT _type, id, space_id, happened_on, description, FORMAT(amount / 100, 2) AS formatted_amount
             FROM (
-                SELECT "earning" AS _type, id, happened_on, description, amount FROM earnings
+                SELECT "earning" AS _type, space_id, id, happened_on, description, amount FROM earnings
                 UNION ALL
-                SELECT "spending" AS _type, id, happened_on, description, amount FROM spendings
+                SELECT "spending" AS _type, space_id, id, happened_on, description, amount FROM spendings
             ) a
+            WHERE space_id = ?
             ORDER BY happened_on DESC
         ';
 
@@ -26,6 +27,6 @@ class TransactionController extends Controller {
         // Close
         $query .= ';';
 
-        return DB::select($query);
+        return DB::select($query, [session('space')->id]);
     }
 }
