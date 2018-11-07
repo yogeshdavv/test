ba367c155b80755e508ffc3fe8e841ab04ec46f3
Daniël van Drunen <daniel@pixely.me>
2018-11-07 18:24:03 +0100

    Add title to transactions' create view

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -1,7 +1,10 @@
 @extends('layout')
 
+@section('title', __('actions.create') . ' Transaction')
+
 @section('body')
     <div class="wrapper my-3">
+        <h2 class="mb-3">{{ __('actions.create') }} Transaction</h2>
         <transaction-wizard></transaction-wizard>
     </div>
 @endsection
