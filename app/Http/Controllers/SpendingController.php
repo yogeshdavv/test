a793a68b0b2f9f77e63ef87d5d7f73d6f5ca8e49
Daniël van Drunen <daniel@pixely.me>
2020-06-10 00:44:06 +0200

    Create SpendingRepository

--- app/Http/Controllers/SpendingController.php
+++ app/Http/Controllers/SpendingController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Spending;
+use App\Repositories\SpendingRepository;
 use Illuminate\Http\Request;
 
-use App\Models\Spending;
-use App\Models\Tag;
-
-use Auth;
-
 class SpendingController extends Controller {
+    private $spendingRepository;
+
+    public function __construct(SpendingRepository $spendingRepository)
+    {
+        $this->spendingRepository = $spendingRepository;
+    }
+
     public function create() {
         $tags = session('space')->tags()->orderBy('created_at', 'DESC')->get();
 
@@ -17,22 +21,15 @@ class SpendingController extends Controller {
     }
 
     public function store(Request $request) {
-        $request->validate([
-            'tag_id' => 'nullable|exists:tags,id', // TODO CHECK IF TAG BELONGS TO USER
-            'date' => 'required|date|date_format:Y-m-d',
-            'description' => 'required|max:255',
-            'amount' => 'required|regex:/^\d*(\.\d{2})?$/'
-        ]);
+        $request->validate($this->spendingRepository->getValidationRules());
 
-        $spending = new Spending;
-
-        $spending->space_id = session('space')->id;
-        $spending->tag_id = $request->input('tag_id');
-        $spending->happened_on = $request->input('date');
-        $spending->description = $request->input('description');
-        $spending->amount = (int) ($request->input('amount') * 100);
-
-        $spending->save();
+        $this->spendingRepository->create(
+            session('space')->id,
+            $request->input('tag_id'),
+            $request->input('date'),
+            $request->input('description'),
+            (int) ($request->input('amount') * 100)
+        );
 
         return redirect()->route('dashboard');
     }
@@ -48,20 +45,14 @@ class SpendingController extends Controller {
     public function update(Request $request, Spending $spending) {
         $this->authorize('update', $spending);
 
-        // TODO MOVE TO CENTRAL PLACE FOR REUSABILITY
-        $request->validate([
-            'tag_id' => 'nullable|exists:tags,id', // TODO CHECK IF TAG BELONGS TO USER
-            'date' => 'required|date|date_format:Y-m-d',
-            'description' => 'required|max:255',
-            'amount' => 'required|regex:/^\d*(\.\d{2})?$/'
-        ]);
+        $request->validate($this->spendingRepository->getValidationRules());
 
-        $spending->fill([
+        $this->spendingRepository->update($spending->id, [
             'tag_id' => $request->input('tag_id'),
             'happened_on' => $request->input('date'),
             'description' => $request->input('description'),
             'amount' => (int) ($request->input('amount') * 100)
-        ])->save();
+        ]);
 
         return redirect()->route('transactions.index');
     }
