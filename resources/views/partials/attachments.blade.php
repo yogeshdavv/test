fbd20b10c6e6ee891a2cdb80f0bf72a5d91f36e1
Daniël van Drunen <daniel@pixely.me>
2020-06-13 20:57:59 +0200

    Create endpoint for uploading attachments

--- resources/views/partials/attachments.blade.php
+++ resources/views/partials/attachments.blade.php
@@ -1,4 +1,10 @@
 <h3 class="mt-3 mb-1">Attachments</h3>
+<form method="POST" action="/attachments">
+    {{ csrf_field() }}
+    <input type="hidden" name="transaction_type" value="{{ get_class($payload) === 'App\Models\Earning' ? 'earning' : 'spending' }}" />
+    <input type="hidden" name="transaction_id" value="{{ $payload->id }}" />
+    <input type="file" name="file" onchange="this.form.submit()" />
+</form>
 <div class="box">
     @if (!$payload->attachments->count())
         <div class="box__section text-center">No attachments for this transaction</div>
